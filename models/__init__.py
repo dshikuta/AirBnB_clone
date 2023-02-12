5fabb5640ba5b93bb8fe2aafd66dbc40e6bb9ab1
dshikuta <dshikuta@gmail.com>
2023-02-12 07:02:44 -0800

    init

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""__init__ magic method for models directory"""
-from models.engine.file_storage import FileStorage
-
-
-=======
-"""Module for Filestorage autoinit."""
+"""Module for FileStorage autoinit."""
 
 from models.engine.file_storage import FileStorage
->>>>>>> 64ea80399742e664f55679d408da6a558695dbae
 storage = FileStorage()
 storage.reload()
