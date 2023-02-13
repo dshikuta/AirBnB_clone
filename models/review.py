d4f6da2fe587168d26f380938c4d8acd4300803c
dshikuta <dshikuta@gmail.com>
2023-02-13 08:45:57 -0800

    Write a command interpreter to manage your AirBnB objects

--- models/review.py
+++ models/review.py
@@ -2,20 +2,10 @@
 
 """Module for Review class."""
 
-
-
 from models.base_model import BaseModel
 
-
-
-
-
 class Review(BaseModel):
-
     """Class representing a Review."""
-
     place_id = ""
-
     user_id = ""
-
     text = ""
